0cd61cd44c30ba6f4e9458f500b53b6a6f43cf4b
82orez <82orez@naver.com>
2024-11-30 09:55:31 +0900

    google start

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,15 +1,16 @@
 import NextAuth, { NextAuthOptions } from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import prisma from "@/lib/prisma";
+import Google from "next-auth/providers/google";
 
 export const authOptions: NextAuthOptions = {
-  session: {
-    strategy: "jwt",
-    maxAge: 60 * 60 * 24,
-    updateAge: 60 * 60 * 2,
-  },
   adapter: PrismaAdapter(prisma),
-  providers: [],
+  providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+    }),
+  ],
   pages: {},
   callbacks: {},
 };
