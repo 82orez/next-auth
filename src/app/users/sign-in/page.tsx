007d71509e3bc1b1ecf82c8e72a385b23398e7d6
82orez <82orez@naver.com>
2024-12-08 20:24:43 +0900

    animate-pulse

--- src/app/users/sign-in/page.tsx
+++ src/app/users/sign-in/page.tsx
@@ -47,9 +47,13 @@ export default function SignInPage() {
     <div>
       {/*UI 기반 오류 메세지 부분*/}
       {error === "alreadyLinked" && provider && (
-        <div className="bg-red-100 text-red-800 p-4 rounded-md">이미 {provider} 로그인으로 가입하셨습니다. 해당 계정으로 로그인해주세요.</div>
+        <div className="bg-red-100 text-red-800 p-4 rounded-md animate-pulse">
+          이미 {provider} 로그인으로 가입하셨습니다. 해당 계정으로 로그인해주세요.
+        </div>
       )}
-      {error === "OAuthCallback" && <div className="bg-red-100 text-red-800 p-4 rounded-md">네트워크 오류입니다. 잠시 후 다시 시도해 주세요.</div>}
+      {error === "OAuthCallback" && (
+        <div className="bg-red-100 text-red-800 p-4 rounded-md animate-pulse">네트워크 오류입니다. 잠시 후 다시 시도해 주세요.</div>
+      )}
 
       <div className="flex flex-col gap-6">
         <Link href={"/"}>Move to Home</Link>
